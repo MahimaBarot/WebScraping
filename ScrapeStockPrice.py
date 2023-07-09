b0cc8e8a84baf2ec9bf753cb16c3ae2d5837ddfc
MahimaBarot <36514613+MahimaBarot@users.noreply.github.com>
2023-07-09 19:26:07 -0400

    Add files via upload

--- ScrapeStockPrice.py
+++ ScrapeStockPrice.py
@@ -2,9 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+# Adding stock names to extract information from
 mystocks = ['%5EIXIC','GC%3DF', 'BTC-USD', '%5ECMC200','%5ERUT']
 stockdata = []
 
+'''
+getrPrice(symbol)
+input:  string form of stock name
+output: dictionary containing stock name , stock price, price change
+'''
 def getPrice(symbol):
 
   headers = {'User-Agent'  : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
@@ -16,15 +22,17 @@ def getPrice(symbol):
   soup = BeautifulSoup(r.text, 'html.parser')
  
   stock = {
-    'symbol': soup.find('div',{'class':'D(ib) '}).find_all('h1')[0].text,
+    'symbol': soup.find('h1',{'class':'D(ib) Fz(18px)'}).text,
     'price' : soup.find('div',{'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text,
     'change' : soup.find('div',{'class':'D(ib) Mend(20px)'}).find_all('span')[1].text
   }
   return stock
 
+#iterate on list of stock names and append information in stockdata
 for item  in mystocks:
   stockdata.append(getPrice(item))
   print('Getting: ', item)
 
+#save information in JSON file
 with open('stockdata.json', 'w') as f:
   json.dump(stockdata, f)
